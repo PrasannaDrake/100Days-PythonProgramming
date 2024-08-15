c88e9481d1a7cf5c23973c867dc088229a768651
Prasanna <ompradhan.prasanna2005@gmail.com>
2024-08-15 08:25:45 +0530

    Comit

---
+++ Day1_HelloWorld.py
@@ -0,0 +1,3 @@
+#Hello World Program
+print("Hello World!")
+print("I'm going to explore the world of Python Programming")
---
+++ Day2.py
@@ -0,0 +1,25 @@
+#Day 2  Comments, Escape Sequences, Print Single-line and Multi-lineStrings
+'''
+Print Poem Fire and Ice by Robert Frost
+Using Escape Sequences
+Using Using Single-line and Multi-line Comment
+'''
+#Use # for Single Line Comments     --> #Comment
+#Use Multi-line commen by enclosing comment within pair of three single quotes or three double quotes   --> '''Comment'''  or  """Comment"""
+print("\t\tFire and Ice\n\t\t          -by Robert Frost")
+
+#Print Multi-line String with single print function by enclosing the text within triple single or double quotes
+print("""
+\n\n
+\tSome say the world will end in fire,
+\tSome say in ice.
+\tFrom what I’ve tasted of desire
+\tI hold with those who favor fire.
+\tBut if it had to perish twice,
+\tI think I know enough of hate
+\tTo say that for destruction ice
+\tIs also great
+\tAnd would suffice.
+""")
+print("\n\n") # Empty line for better readability
+print("\"Coding like poetry should be short and concise.\" \n                                    ― Santosh Kalwar")
